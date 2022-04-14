80ecd1d2913c023183aa759076a69845a826989c
pepinodev <92928197+pepinodev@users.noreply.github.com>
2022-04-14 18:54:43 -0300

    feat: componente card criado e estilizado

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Router from "./Pages/Router/Router";
 import Header from "./Components/PasteHeader/Header";
+import Card from "./Components/PasteCard/Card";
 
 function App() {
   return (
     <div>
       <Header />
       <Router />
+      <Card />
     </div>
   );
 }
---
+++ pokedex/src/Components/PasteCard/StyledCard.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+
+export const GeneralStyledDiv = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+` 
+export const BiggerContainer = styled.div`
+  background-color: #eff0f4;
+  width: 33vh;
+  height: 40vh;
+  box-shadow: 0px 0px 5px;
+  padding: 10px;
+  border-radius: 5px;
+`;
+export const SmallerContainer = styled.div`
+ display: flex;
+ justify-content: center;
+ margin-top: 30px;
+ width: 50%;
+ height: 50%;
+`;
+
+export const StatsCard = styled.div`
+ background-color: white;
+ justify-content: center;
+ width: 200px;
+ height: 100px;
+ border-radius: 5px;
+`

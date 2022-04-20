6be72302f4058eb06c308bd0cfb31f8cdb8a305a
milenyfaria <myrfaria@gmail.com>
2022-04-20 19:02:29 -0300

    voltando estados e requisicoes para home

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Router from "./Pages/Router/Router";
 import Header from "./Components/PasteHeader/Header";
-import GlobalState from './global/GlobalState'
 
 function App() {
   return (
-    <GlobalState>
+    <>
        <Header />
       <Router />
-    </GlobalState>
+    </>
   );
 }
 
--- pokedex/src/Pages/PasteHomePage/HomePage.js
+++ pokedex/src/Pages/PasteHomePage/HomePage.js
@@ -1,9 +1,10 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import {useNavigate} from "react-router-dom"
 import Card from "../../components/PasteCard/Card"
 import {goToPokedexPage, goToDetailPage} from "../Router/Coodinator"
 import styled from "styled-components";
-import { GlobalStateContext } from '../../global/GlobalStateContext'
+import axios from "axios"
+import { BASE_URL } from "../constants/url"
 
 const Container = styled.div`
     display: flex;
@@ -16,18 +17,25 @@ const Container = styled.div`
 
 export function HomePage (){
     const navigate = useNavigate()
-    const { states, setters, requests } = useContext(GlobalStateContext)
-    const pokelist = states
-    const listPokemons = requests
-    const pokedex = states
-    const setPokedex = setters
+    const [ pokelist, setPokelist ] = useState([])
+    const [ pokedex, setPokedex] = useState([])
 
     useEffect(() => {
        listPokemons()
 
     }, [])
 
-    console.log(pokelist)
+    const listPokemons = () => {
+        axios
+            .get(`${BASE_URL}?limit=20`)
+            .then((res) => {
+                setPokelist(res.data.results)
+            })
+            .catch((err) => {
+                alert("Deu erro!")
+            })
+    }
+
 
     const addPokeToPokedex = (newPoke) => {
         const newPokedex = [...pokedex]
